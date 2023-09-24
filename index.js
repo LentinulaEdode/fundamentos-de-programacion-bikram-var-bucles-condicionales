2a2b634736f6f47159c5cebf415e3c61a326b10b
LentinulaEdode <carmona.a.esther@gmail.com>
2023-09-24 15:18:41 +0200

    new file:   Ejercicio25.PNG 	new file:   ejercicio24.PNG 	new file:   ejercicio26.PNG 	modified:   index.js 	new file:   victoryy.PNG

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ for (let i = 0; i < 10; i++) {
 // 13. 
 let variableValorNumerico = 4;
 // 14. 
-const MiNombre = ther; 
+const MiNombre = 'ther'; 
 // 15. 
 const MiNumeroFav = 9; 
 // 16. 
@@ -49,7 +49,33 @@ let booleanoOr = (booleano1||booleano2);
 // 17. 
 let booleanoMix1 = (booleano1 && (TAU/2 ===PI)) || (variableValorNumerico >= MiNumeroFav);
 // 18. 
-
-
-
+let seisNoEsNueve = (6!==9); 
+// 19. 
+let booleanoMix2 = variableValorNumerico > 0 || variableValorNumerico < (MiNumeroFav*TAU);
+// 20. 
+let valorSuma = (MiNumeroFav + variableValorNumerico);
+// 21. 
+let valorResta = (MiNumeroFav - variableValorNumerico);
+// 22. 
+let valorMultiplicacion = (MiNumeroFav*variableValorNumerico);
+// 23. 
+let valorDivision = (MiNumeroFav/3);
+// 24. 
+let contarHasta10 = 0;
+while (contarHasta10 < 10) {
+    (contarHasta10++);
+} 
+// 25. 
+let preI = 0;
+let preJ = 0; 
+for (let i = 0; i < 11; i++) {
+    preI+= ++preJ;
+    }
+// 26. 
+let sumaImpares = 0; 
+for (let i = 0; i < 10; i++) {
+    if (i % 2 === 1) {
+    sumaImpares += i;
+    }
+}
 
